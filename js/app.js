b357fbf417a41c0db14439245be2c706116b8523
Jessica Hansen <jesi.hester@gmail.com>
2026-03-15 15:44:32 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
+const rigArray = {
+   designator : "",    //STR - a98,etc
+   status : "",        //available/txp/etc
+   nextTrip : "",      //next planned pickup
+   tripDuration : "",  //next trip duration
+   estNext : "",       //estimated next ETA
+   lunch ; ""          //INT - has this unit had a lunch
+}
+/*
+STATUSES
+AT LUNCH
+QUICK DETAIL
+AVAILABLE
+EN ROUTE
+ON SCENE
+TRANSPORTING
+AT DESTINATION
+NOT IN SERVICE
+*/
+
 window.addEventListener('DOMContentLoaded', (event) => {
+    var a96 = {}
     console.log('DOM fully loaded and parsed');
 });
 
@@ -12,9 +33,12 @@ document.getElementById("county").addEventListener('change', (event) => {
     	//ONLY SHOW THAT COUNTY'S RIGS
     var rigSwitch = document.getElementByClassName("rigs");
     
+//show selected county rigs only in table
+//NOT WORKING YET
     if (rigSwitch.display === "none") {
 	  rigSwitch.display = "block";
 	} else {
 	  rigSwitch.display = "none";
 	}
   });
+
