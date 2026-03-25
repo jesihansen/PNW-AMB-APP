2f73f1c693b4b86084b3a5124c085eef7b6467dd
Jessica Hansen <jesi.hester@gmail.com>
2026-03-24 18:58:31 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,50 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+document.getElementById("county").addEventListener('change', (event) => {
+	//ON CHANGE, GET DATA
+    var selectedOption = event.target.options[event.target.selectedIndex];
+    	//STORE COUNTY
+    var targetData = selectedOption.getAttribute('data-target');
+    	//LOG
+    console.log(targetData);
+    	//ONLY SHOW THAT COUNTY'S RIGS
+    toggleVisibility(targetData);
+    
+//show selected county rigs only in table
+    
+  });
+
+function toggleVisibility(county) {
+// GET ALL OPTIONS
+ const rigSwitch = document.getElementsByClassName(county + "-rig");
+// SHOW THAT WE HAVE THEM 
+ console.log(rigSwitch);
+// ADD OR REMOVE SHOW
+ rigSwitch.forEach(showHide());
+ 
+}
+
+function showHide(rig){
+console.log("in showHide");
+	//BROKEN RN
+ if (rig.style.display === "none") {
+      console.log(this);
+      rig.style.display = "block !important";
+   } else {
+      rig.style.display = "none";
+   }
+}
+
+
 const rigArray = {
    designator : "",    //STR - a98,etc
    status : "",        //available/txp/etc
    nextTrip : "",      //next planned pickup
    tripDuration : "",  //next trip duration
    estNext : "",       //estimated next ETA
-   lunch ; ""          //INT - has this unit had a lunch
+   lunch : ""          //INT - has this unit had a lunch
 }
 /*
 STATUSES
@@ -17,28 +57,3 @@ TRANSPORTING
 AT DESTINATION
 NOT IN SERVICE
 */
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    var a96 = {}
-    console.log('DOM fully loaded and parsed');
-});
-
-document.getElementById("county").addEventListener('change', (event) => {
-	//ON CHANGE, GET DATA
-    var selectedOption = event.target.options[event.target.selectedIndex];
-    	//STORE COUNTY
-    var targetData = selectedOption.getAttribute('data-target');
-    	//LOG
-    console.log(targetData);
-    	//ONLY SHOW THAT COUNTY'S RIGS
-    var rigSwitch = document.getElementByClassName("rigs");
-    
-//show selected county rigs only in table
-//NOT WORKING YET
-    if (rigSwitch.display === "none") {
-	  rigSwitch.display = "block";
-	} else {
-	  rigSwitch.display = "none";
-	}
-  });
-
